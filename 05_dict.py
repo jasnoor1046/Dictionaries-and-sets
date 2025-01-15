521ae9bb4231702c403c1d2d9b4706d01045b7a0
Jasnoor Kaur <jasnoor00070@gmail.com>
2025-01-15 21:33:30 +0530

    dictionary methods part 3

---
+++ 05_dict.py
@@ -0,0 +1,10 @@
+# dictionary methods part 3
+# clear
+dict1 = {"a": 1, "b": 2, "c": 3}
+dict1 = dict1.clear()
+print(dict1)
+
+# to print the items in a dictionary
+my_dict = {"city": "new york", "name": "noor", "age": "18"}
+items1 = my_dict.items()
+print(items1)
